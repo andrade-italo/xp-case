c589c1ddded846ad9fa351976a2c801c09903af4
Italo Andrade <italoandrade13@gmail.com>
2022-07-23 12:39:52 -0300

    fix: clientService get all ativos inside carteira by codClient.

--- src/database/services/clientService.js
+++ src/database/services/clientService.js
@@ -1,7 +1,10 @@
-const { Carteiras } = require('../models');
+const { sequelize } = require('../models');
 
 const clientService = async (codCliente) => {
-  const getById = await Carteiras.findOne({ where: { codCliente } });
-  return getById;
+  const [raw] = await sequelize.query(`SELECT "Carteiras"."cod_cliente" AS "codCliente", "Carteiras"."cod_ativo" AS
+     "codAtivo", "Carteiras"."qtde_ativo" AS "qtdeAtivo", "Ativos"."valor" AS "valor" FROM
+      "Carteiras" INNER JOIN "Ativos" ON "Carteiras"."cod_ativo" = "Ativos"."cod_ativo" WHERE 
+      "Carteiras"."cod_cliente" = ?`, { replacements: [codCliente] });
+  return raw;
 };
 module.exports = clientService;
