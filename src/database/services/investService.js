4edf45703ecd9196672573b1eafab2b48ff1dd01
Italo Andrade <italoandrade13@gmail.com>
2022-07-23 00:15:50 -0300

    feat: add validation of balance

--- src/database/services/investService.js
+++ src/database/services/investService.js
@@ -3,9 +3,12 @@ const { Carteiras, Ativos } = require('../models');
 const investService = async (payload) => {
   const { codAtivo, qtdeAtivo, codCliente } = payload;
   const findAtivo = await Ativos.findOne({ where: { cod_ativo: codAtivo } });
+  const findCliente = await Clientes.findOne({ where: { cod_cliente: codCliente } });
+  const totalCompra = (findAtivo.valor * qtdeAtivo).toFixed(2);
   if (!findAtivo) {
     return { message: 'Ativo não encontrado' };
   }
+  if (findCliente.saldo < totalCompra) return { message: 'Saldo insuficiente!' };
   if (findAtivo.qtdeAtivo < qtdeAtivo) return { message: 'Quantidade indisponível' };
   const created = Carteiras.create({ codAtivo, codCliente, qtdeAtivo });
   return created;
