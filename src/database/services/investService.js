0a30b1e4bd36bc9969229a2c393731dbdb61d9c2
Italo Andrade <italoandrade13@gmail.com>
2022-07-23 01:01:30 -0300

    feat: extract validations for middleware

--- src/database/services/investService.js
+++ src/database/services/investService.js
@@ -5,9 +5,7 @@ const investService = async (payload) => {
   const findAtivo = await Ativos.findOne({ where: { cod_ativo: codAtivo } });
   const findCliente = await Clientes.findOne({ where: { cod_cliente: codCliente } });
   const totalCompra = (findAtivo.valor * qtdeAtivo).toFixed(2);
-  if (!findAtivo) {
-    return { message: 'Ativo não encontrado' };
-  }
+
   if (findCliente.saldo < totalCompra) return { message: 'Saldo insuficiente!' };
   if (findAtivo.qtdeAtivo < qtdeAtivo) return { message: 'Quantidade indisponível' };
 
