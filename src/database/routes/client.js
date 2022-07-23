f4e4deae998b4eb4f69a2bc362df606da6d4e677
Italo Andrade <italoandrade13@gmail.com>
2022-07-23 13:15:36 -0300

    feat: add middleware to route get client/ativo/:codAtivo

--- src/database/routes/client.js
+++ src/database/routes/client.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const clientController = require('../controllers/clientController');
+const validateGetClient = require('../middleware/validateGetClient');
 
 const router = express.Router();
 
-router.get('/ativos/:codCliente', clientController);
+router.get('/ativos/:codCliente', validateGetClient, clientController);
 
 module.exports = router;
