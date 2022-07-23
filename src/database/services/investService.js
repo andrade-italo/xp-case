c5bec09ea13ec264e5c26e538611a641fdf0d8c8
Italo Andrade <italoandrade13@gmail.com>
2022-07-23 01:03:23 -0300

    fix: Carteiras referr codCliente now

--- src/database/services/investService.js
+++ src/database/services/investService.js
@@ -9,7 +9,9 @@ const investService = async (payload) => {
   if (findCliente.saldo < totalCompra) return { message: 'Saldo insuficiente!' };
   if (findAtivo.qtdeAtivo < qtdeAtivo) return { message: 'Quantidade indisponível' };
 
-  const findCarteira = await Carteiras.findOne({ where: { cod_ativo: codAtivo } });
+  const findCarteira = await Carteiras.findOne(
+    { where: { cod_ativo: codAtivo, cod_cliente: codCliente } },
+  );
 
   if (!findCarteira) {
     const created = await Carteiras.create({ codAtivo, codCliente, qtdeAtivo });
@@ -22,7 +24,7 @@ const investService = async (payload) => {
 
   const updateCarteira = await Carteiras.increment(
     { qtde_ativo: +qtdeAtivo },
-    { where: { cod_ativo: codAtivo } },
+    { where: { cod_ativo: codAtivo, cod_cliente: codCliente } },
   );
   return updateCarteira;
 };
