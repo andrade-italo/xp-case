f1086d6c5c4d83e4bce16a08c0e845a7a72352b1
Italo Andrade <italoandrade13@gmail.com>
2022-07-23 13:29:51 -0300

    feat: add all layer of get conta/:codCliente

--- src/app.js
+++ src/app.js
@@ -5,10 +5,12 @@ const app = express();
 app.use(express.json());
 const investimentos = require('./database/routes/investimentos');
 const client = require('./database/routes/client');
+const conta = require('./database/routes/conta');
 const assets = require('./database/routes/assets');
 
 app.use('/investimentos', investimentos);
 app.use('/client', client);
 app.use('/assets', assets);
+app.use('/conta', conta);
 
 module.exports = app;
