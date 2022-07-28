f2544e51ff8d277f8b9f48b7fed1dcd394193f30
Italo Alves de Andrade <italoandrade13@gmail.com>
2022-07-28 11:27:11 -0300

    fix: validação na rota (requerida na entrevista)

--- src/database/routes/assets.js
+++ src/database/routes/assets.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const assetsController = require('../controllers/assetsController');
 const validateGetAssets = require('../middleware/validateGetAssets');
+const { authToken } = require('../middleware/authToken');
 
 const router = express.Router();
 
 router.get('/ativos/:codAtivo', validateGetAssets, assetsController.assetsController);
-router.post('/ativos', assetsController.addAtivoController);
+router.post('/ativos', authToken, assetsController.addAtivoController);
 
 module.exports = router;
