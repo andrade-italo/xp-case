d610583022a2dcd932ec0ed315555e0afa8c25f0
Italo Alves de Andrade <italoandrade13@gmail.com>
2022-07-28 10:48:05 -0300

    update route

--- src/database/routes/assets.js
+++ src/database/routes/assets.js
@@ -5,6 +5,6 @@ const validateGetAssets = require('../middleware/validateGetAssets');
 const router = express.Router();
 
 router.get('/ativos/:codAtivo', validateGetAssets, assetsController.assetsController);
-router.post('/ativos/:codAtivo', assetsController.addAtivoController);
+router.post('/ativos', assetsController.addAtivoController);
 
 module.exports = router;
